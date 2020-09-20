648e6caebb18b1f64a56ed87b6077fe2094dfa17
abraun20 <allanvobraun@gmail.com>
2020-09-20 16:15:37 -0300

    Arrumado url axios para ambiente

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
-
 import 'styles/_app.scss';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
@@ -10,6 +11,12 @@ import store from './store';
 
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
+const axiosConfig = {
+  baseURL: process.env.API_URL,
+  timeout: 30000,
+};
+
+Vue.use(VueAxios, axios.create(axiosConfig));
 
 new Vue({
   router,
