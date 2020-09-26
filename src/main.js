bc9d013665a4bc21ac0c460fb945d635bbf0f119
abraun20 <allanvobraun@gmail.com>
2020-09-26 18:53:40 -0300

    Adicionada biblioteca de notificação ,

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@ import Vue from 'vue';
 import VueMaterial from 'vue-material';
 import ax from 'config/axiosConfig';
 import VueAxios from 'vue-axios';
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue';
 import 'styles/_app.scss';
-import 'vue-material/dist/vue-material.min.css';
-import 'vue-material/dist/theme/default.css';
 import router from './router';
 import store from './store';
 import validationRules from './validation';
 import helpers from './helpers';
+
+Vue.use(VueSweetalert2);
 
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
