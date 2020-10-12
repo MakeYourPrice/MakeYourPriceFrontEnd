83ba194418b2e7a9fb0116ade81e33d93a937a9a
abraun20 <allanvobraun@gmail.com>
2020-10-12 15:20:39 -0300

    Removida dependencia do wrapper do sweet alert2

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueMaterial from 'vue-material';
 import ax from 'config/axiosConfig';
 import VueAxios from 'vue-axios';
-import VueSweetalert2 from 'vue-sweetalert2';
+import Swal from 'sweetalert2';
 import App from './App.vue';
 import 'styles/_app.scss';
 import router from './router';
@@ -15,13 +15,20 @@ require('./errorHandling');
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
 Vue.use(VueAxios, ax);
-Vue.use(VueSweetalert2);
 
 // helpers
 Vue.use({
   install() {
     Vue.helpers = helpers;
     Vue.prototype.$helpers = helpers;
+  },
+});
+
+// sweet alert
+Vue.use({
+  install() {
+    Vue.swal = Swal;
+    Vue.prototype.$swal = Swal;
   },
 });
 
