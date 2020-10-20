abd4d307d993c8616db13e48bde0df053061e45b
abraun20 <allanvobraun@gmail.com>
2020-10-20 18:30:06 -0300

    Commit intermediario tela produtos

---
+++ src/components/products/Product.vue
@@ -0,0 +1,28 @@
+<template>
+  <md-list-item>
+    <md-avatar>
+      <ftp-image img-path="product.productImage"></ftp-image>
+    </md-avatar>
+
+    <div class="md-list-item-text">
+      <span>{{ productObject.name }}</span>
+      <span>{{ productObject.productBrand }}</span>
+      <span>{{ productObject.productBrand}}</span>
+    </div>
+
+  </md-list-item>
+</template>
+<script>
+import FtpImage from 'components/FtpImage';
+
+export default {
+  name: 'itemProduto',
+  components: { FtpImage },
+  props: {
+    productObject: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/components/products/ProductsList.vue
@@ -0,0 +1,67 @@
+<template>
+  <md-list class="md-triple-line">
+    <product productObject="{}"></product>
+
+    <md-divider class="md-inset"></md-divider>
+
+    <md-list-item>
+      <md-avatar>
+        <img src="http://lorempixel.com/40/40/food/2" alt="People">
+      </md-avatar>
+
+      <div class="md-list-item-text">
+        <span>Feijão</span>
+        <span>Do zé</span>
+        <span>Granulados</span>
+      </div>
+
+      <md-button class="md-icon-button md-list-action">
+        <md-icon>star_border</md-icon>
+      </md-button>
+    </md-list-item>
+
+    <md-divider class="md-inset"></md-divider>
+
+    <md-list-item>
+      <md-avatar>
+        <img src="http://lorempixel.com/40/40/food/3" alt="People">
+      </md-avatar>
+
+      <div class="md-list-item-text">
+        <span>Batata</span>
+        <span>Da granja</span>
+        <span>Vegetais</span>
+      </div>
+
+      <md-button class="md-icon-button md-list-action">
+        <md-icon>star_border</md-icon>
+      </md-button>
+    </md-list-item>
+
+    <md-divider class="md-inset"></md-divider>
+
+    <md-list-item>
+      <md-avatar>
+        <img src="http://lorempixel.com/40/40/food/4" alt="People">
+      </md-avatar>
+
+      <div class="md-list-item-text">
+        <span>Arroz</span>
+        <span>Do Marcio</span>
+        <span>Comida</span>
+      </div>
+
+      <md-button class="md-icon-button md-list-action">
+        <md-icon>star_border</md-icon>
+      </md-button>
+    </md-list-item>
+  </md-list>
+</template>
+<script>
+import Product from 'components/products/Product';
+
+export default {
+  name: 'ProductsList',
+  components: { Product },
+};
+</script>
