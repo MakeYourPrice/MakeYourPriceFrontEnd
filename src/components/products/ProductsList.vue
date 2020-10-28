2221c4259561223fd198137d85d4cd7e44c47afb
Allan Braun <allanvobraun@gmail.com>
2020-10-28 00:16:15 -0300

    Listagem dos produtos

--- src/components/products/ProductsList.vue
+++ src/components/products/ProductsList.vue
@@ -1,60 +1,9 @@
 <template>
   <md-list class="md-triple-line">
-    <product productObject="{}"></product>
-
-    <md-divider class="md-inset"></md-divider>
-
-    <md-list-item>
-      <md-avatar>
-        <img src="http://lorempixel.com/40/40/food/2" alt="People">
-      </md-avatar>
-
-      <div class="md-list-item-text">
-        <span>Feijão</span>
-        <span>Do zé</span>
-        <span>Granulados</span>
-      </div>
-
-      <md-button class="md-icon-button md-list-action">
-        <md-icon>star_border</md-icon>
-      </md-button>
-    </md-list-item>
-
-    <md-divider class="md-inset"></md-divider>
-
-    <md-list-item>
-      <md-avatar>
-        <img src="http://lorempixel.com/40/40/food/3" alt="People">
-      </md-avatar>
-
-      <div class="md-list-item-text">
-        <span>Batata</span>
-        <span>Da granja</span>
-        <span>Vegetais</span>
-      </div>
-
-      <md-button class="md-icon-button md-list-action">
-        <md-icon>star_border</md-icon>
-      </md-button>
-    </md-list-item>
-
-    <md-divider class="md-inset"></md-divider>
-
-    <md-list-item>
-      <md-avatar>
-        <img src="http://lorempixel.com/40/40/food/4" alt="People">
-      </md-avatar>
-
-      <div class="md-list-item-text">
-        <span>Arroz</span>
-        <span>Do Marcio</span>
-        <span>Comida</span>
-      </div>
-
-      <md-button class="md-icon-button md-list-action">
-        <md-icon>star_border</md-icon>
-      </md-button>
-    </md-list-item>
+    <div class="product-space" v-for="product in products" :key="product.id">
+      <product :productObject="product" ></product>
+      <md-divider class="md-inset"></md-divider>
+    </div>
   </md-list>
 </template>
 <script>
@@ -63,5 +12,15 @@ import Product from 'components/products/Product';
 export default {
   name: 'ProductsList',
   components: { Product },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  mounted() {
+    this.axios.get('/products').then((response) => {
+      this.products = response.data;
+    });
+  },
 };
 </script>
